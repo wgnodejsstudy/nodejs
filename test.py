6809c7881fa4897822cf9acc2522aa654f70090b
voyage2510 <yujs30@snu.ac.kr>
2019-07-23 14:57:00 +0900

    commit from github site directly
    propose new file?

---
+++ test.py
@@ -0,0 +1,12 @@
+def Test(object):
+    def __init__(self, c):
+        self.c = c
+    def __cal__(c):
+        self.c = c
+        print(f'member c has changed to {c}')
+
+
+if __name__=='__main__':
+    print(f'Hello World!')
+    dgithub = Test(3)
+    dgithub(365)
