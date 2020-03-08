304f427dbc8b336849f40118df23e969ea54a3a7
jinsub-kim <less8430@gmail.com>
2020-03-08 15:20:10 +0900

    Node 서버와 React 클라이언트 연동

--- PathOfExile/poe-trade/servers/server.js
+++ PathOfExile/poe-trade/servers/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
+const route = require('./routes/index');
 
 app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'Path of Exile'}));
+// app.use('/api', (req, res)=> res.json({username:'Path of Exile'}));
+app.use('/api', route);
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
--- PathOfExile/poe-trade/src/App.js
+++ PathOfExile/poe-trade/src/App.js
@@ -10,6 +10,12 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('api')
+      .then(res=>res.json())
+      .then(data=>this.setState({username:data.username}));
+  }
+
   render() {
     const {username} = this.state;
     return (
